b35debdb6ce666e1f2b73f4c6292b3fd38b0b311
Armin Troy <mail@technicalagile.coach>
2024-08-28 17:29:17 +0200

    Simplified StringToInt and IntToString

--- SudokuSolverCore/CellHelpers.cs
+++ SudokuSolverCore/CellHelpers.cs
@@ -4,20 +4,16 @@ internal static class CellHelpers
 { 
     internal static string IntToString(int value)
     {
-        string s;
         if (value == UNDEFINED)
-            s = " ";
-        else
-            s = value.ToString();
-        return s;
-    }
-    internal static int StringToInt(string simplePuzzle1)
-    {
-        int result = UNDEFINED;
-        var ch = simplePuzzle1.ToString();
+            return " ";
 
-        if (ch != " ")
-            result = Int32.Parse(ch);
-        return result;
+        return value.ToString();
+    }
+    internal static int StringToInt(string value)
+    {
+        if (value == " ")
+            return UNDEFINED;
+
+        return Int32.Parse(value);
     }
 }
