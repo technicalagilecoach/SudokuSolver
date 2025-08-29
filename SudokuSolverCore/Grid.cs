1332f843ff5470b0a0cf990f60654218a05fedd5
Armin Troy <mail@technicalagile.coach>
2025-08-29 14:18:45 +0200

    Remove unused import directives.

--- SudokuSolverCore/Grid.cs
+++ SudokuSolverCore/Grid.cs
@@ -1,6 +1,4 @@
-﻿using System.Data.Common;
-
-namespace SudokuSolverCore
+﻿namespace SudokuSolverCore
 {
     public class Grid
     {
--- SudokuSolverCore/Solver.cs
+++ SudokuSolverCore/Solver.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using static SudokuSolverCore.Grid;
+﻿using static SudokuSolverCore.Grid;
 
 namespace SudokuSolverCore
 {
